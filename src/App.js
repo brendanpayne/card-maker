6cdc6817eab81ddc2fb93f134fc281f25d0b84a8
brendanpayne <payneb2@mail.uc.edu>
2023-10-09 16:21:58 -0400

    basic card making logic implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import html2canvas from 'html2canvas';
 import './App.css';
 
-function App() {
+function CardGenerator() {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [image, setImage] = useState('');
+  const [description, setDescription] = useState('');
+  const [group, setGroup] = useState('');
+  const cardRef = useRef(null);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const cardName = `${number} - ${name}`;
+    // Store the information in state
+  };
+
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      setImage(event.target.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+  const handleGroupChange = (event) => {
+    setGroup(event.target.value);
+  };
+
+  const handleDownload = () => {
+    html2canvas(cardRef.current).then((canvas) => {
+      const link = document.createElement('a');
+      link.download = number ? `${number} - ${name}` : 'card.png';
+      link.href = canvas.toDataURL();
+      link.click();
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
+        </label>
+        <label>
+          Number:
+          <input type="number" value={number} onChange={(event) => setNumber(event.target.value)} />
+        </label>
+        <label>
+          Group:
+          <select value={group} onChange={handleGroupChange}>
+            <option value="">Basic</option>
+            <option value="otto">Otto</option>
+            <option value="bushido">Bushido</option>
+            <option value="rat">Rat</option>
+            <option value="shop">Shop</option>
+            <option value="realm">Realm</option>
+          </select>
+        </label>
+        <label>
+          Image:
+          <input type="file" onChange={handleImageUpload} />
+        </label>
+        <label>
+          Description:
+          <textarea value={description} onChange={(event) => setDescription(event.target.value)} />
+        </label>
+        <button type="submit">Generate Card</button>
+      </form>
+      <div className="card" ref={cardRef}>
+        <div className="card-header">
+          <h2 className={`card-name ${group}`}>{number} - {name}</h2>
+          {group && <p className={`card-group ${group}`}>{group}</p>}
+        </div>
+        {image && (
+          <img className='card-image' src={image} alt={name} />
+        )}
+        <p className="card-description">{description}</p>
+      </div>
+      <button onClick={handleDownload}>Download Card</button>
     </div>
   );
 }
 
-export default App;
+export default CardGenerator;
